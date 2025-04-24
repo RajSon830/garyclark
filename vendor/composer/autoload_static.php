d970f6942c391933c892582c1c8f13553ebbbc51
RajSon830 <rajsonawala2023@gmail.com>
2025-04-24 22:42:59 +0200

    Start to work on routing

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,6 +8,7 @@ class ComposerStaticInit48b6b802599e1e99010344fb382f1c55
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
         '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
     );
 
@@ -20,6 +21,10 @@ class ComposerStaticInit48b6b802599e1e99010344fb382f1c55
         'R' => 
         array (
             'Raj\\Framework\\' => 14,
+        ),
+        'F' => 
+        array (
+            'FastRoute\\' => 10,
         ),
         'A' => 
         array (
@@ -39,6 +44,10 @@ class ComposerStaticInit48b6b802599e1e99010344fb382f1c55
         'Raj\\Framework\\' => 
         array (
             0 => __DIR__ . '/../..' . '/framework',
+        ),
+        'FastRoute\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/nikic/fast-route/src',
         ),
         'App\\' => 
         array (
